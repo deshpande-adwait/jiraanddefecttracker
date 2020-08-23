2ff4677abec37fb9f6cc89dc6fe09bbf1db3bc61
Adwait Deshpande <deshpande_adwait@ymail.com>
2020-08-23 14:58:37 +0530

    Login page activated

--- tracker-UI/src/app/app.module.ts
+++ tracker-UI/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { routing } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './servicesapi/LoginService';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    ReactiveFormsModule,
+    CommonModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    HttpHandler,
+    HttpClient,
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
